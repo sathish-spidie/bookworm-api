d5b0a750e0014c257877929008e40005de2cad81
sathish-spidie <sathishshaj9484@gmail.com>
2020-04-07 11:39:06 +0530

    Implemented with express

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.post("/api/auth", (req, res) => {
+  res.status(400).json({
+    errors: {
+      global: "invalid Credentials",
+    },
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("server is running on port 5000");
+});
